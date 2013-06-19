ba0505a1bfb20b169fe9d7885f72f5507c040bb3
Jon Saints <jon@imidata.com>
2013-06-19 12:27:08 -0600

    working example for counties

--- js/main.js
+++ js/main.js
@@ -14,9 +14,10 @@ var svg = d3.select("#home").append("svg")
     .attr("height", height);
 
 queue()
-    //.defer(d3.json, "/js/us-10m.json")
-    .defer(d3.json, "/js/ne_10m_us_states_lakes.json")
-    .defer(d3.json, "http://rocky-plateau-7183.herokuapp.com/1/demand?group_by=state&geo=US&products=90A,90B")
+    .defer(d3.json, "/js/us-10m.json")
+    //.defer(d3.json, "/js/ne_10m_us_states_lakes.json")
+    //.defer(d3.json, "http://rocky-plateau-7183.herokuapp.com/1/demand?group_by=state&geo=US&products=1%2C2%2C3%2C4")
+    .defer(d3.json, "http://0.0.0.0:5000/1/demand?group_by=county&geo=US&products=1%2C2%2C3%2C4")
     .await(ready);
 
 function ready(error, us, demand) {
@@ -24,11 +25,13 @@ function ready(error, us, demand) {
   domain = [];
   results = d3.map();
   for (_i = 0, _len = demand.results.length; _i < _len; _i++) {
-    DEMAND = 3;
-    ABBREV = 2;
+    DEMAND = 4;
+    ABBREV = 3;
     domain.push( demand.results[_i][DEMAND] );
     results.set(demand.results[_i][ABBREV],demand.results[_i][DEMAND]);
   }
+
+  console.log(results);
 
   var quantize = d3.scale.quantize()
     .domain([domain[0],domain[domain.length-1]])
@@ -39,13 +42,27 @@ function ready(error, us, demand) {
     .range(d3.range(9).map(function(i) { return "state q" + i + "-9"; }));
 
   svg.append("g")
+      .attr("class", "county")
+    .selectAll("path")
+      .data(topojson.feature(us, us.objects.counties).features)
+    .enter().append("path")
+      .attr("class", function(d) { fips = d.id+""; while (fips.length < 5) {fips = '0' + fips}; return quantile(results.get(fips)); })
+      .attr("id", function(d) { fips = d.id+""; while (fips.length < 5) {fips = '0' + fips}; return "county-"+fips; } )
+      .attr("d", path);
+
+  /*svg.append("g")
       .attr("id", "states")
     .selectAll("path")
       .data(topojson.feature(us, us.objects.states).features)
     .enter().append("path")
       .attr("d", path)
       .attr("id", function(d) { fips = d.id+""; return "state-"+fips; } )
-      .attr("class", function(d) { return quantile(results.get(d.id)); });
+      .attr("class", function(d) { return quantile(results.get(d.id)); });*/
+
+  /*svg.append("path")
+      .datum(topojson.feature(us, us.objects.counties, function(a, b) { return a === b; }))
+      .attr("d", path )
+      .attr("class", "county-border");*/
 
   svg.append("path")
       .datum(topojson.feature(us, us.objects.states))
